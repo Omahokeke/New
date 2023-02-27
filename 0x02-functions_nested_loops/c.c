745e6bcf38ead7ed007a62795e78aad994d8ee79
Omahokeke <omahokeke@yahoo.com>
2023-02-27 21:44:48 +0000

    Pointers Arrays Strings

---
+++ 0x02-functions_nested_loops/c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int count = 1;
+	char x;
+
+	while(count++ <= 10)
+
+	for(x = 'a'; x <= 'z'; x++)
+	{
+		printf("%c", x);
+	}
+}
---
+++ 0x02-functions_nested_loops/e.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	if (n < 0)
+	{
+		printf("%d\n", n * -1);
+	}
+	else
+	{
+		printf("%d\n");
+	}
+}
