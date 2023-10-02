abb6fe716068804a2f010703ce00a5bc7775c0b7
Omahokeke <Omahokeke@yahoo.com>
2023-10-02 15:07:53 +0000

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Prints out the name of the program
+ * @argc: Number of arguments
+ * @argv: Array of characters
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[]) {
 	if (argc >= 1) {
 		printf("%s\n", argv[0]);
