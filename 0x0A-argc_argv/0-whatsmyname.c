62f855414aa3a6b5495701af68cfc7ff895a93ff
Omahokeke <Omahokeke@yahoo.com>
2023-10-02 15:55:50 +0000

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[]) {
-	if (argc >= 1) {
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+
 		printf("%s\n", argv[0]);
 	}
 
-	return 0;
+	(return 0);
 }
